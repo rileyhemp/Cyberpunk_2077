9cbb57e0966b9f6649f825c13079598ff5e6bdee
Riley Hemphill <rileyhemp@gmail.com>
2019-09-08 04:27:31 -0400

    Working responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,7 +1,15 @@
 "use strict";
 
+var spacer = document.querySelector('.platform-spacer');
 window.addEventListener('resize', function () {
   var vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', "".concat(vh, "px"));
+  resizeSpacer();
 });
+
+function resizeSpacer() {
+  spacer.style.width = window.innerWidth - spacer.getBoundingClientRect().x - 415 + 'px';
+}
+
+resizeSpacer();
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,13 @@
-window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+let spacer = document.querySelector('.platform-spacer')
 
+window.addEventListener('resize', () => {
+	let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    resizeSpacer()
+});
+
+function resizeSpacer() {
+	spacer.style.width = window.innerWidth - spacer.getBoundingClientRect().x - 415 + 'px'
+}
+
+resizeSpacer()
