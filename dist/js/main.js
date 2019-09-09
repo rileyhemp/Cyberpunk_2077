2460b67226b29a383776a482903ef734bf2d1f0d
Riley Hemphill <rileyhemp@gmail.com>
2019-09-08 23:31:19 -0400

    Fixed bugs in responsiveness at higher resolutions

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,19 +31,20 @@ if (window.innerWidth >= 1024) {
         color: 'black',
         opacity: 0
       }
-    }, 0.02, "-=1").staggerFrom(splitParagraph.lines, 1, {
+    }, 0.02, "-=2").staggerFrom(splitParagraph.lines, 1, {
       ease: Power2.easeInOut,
       css: {
         opacity: 0,
         height: 0,
         overflow: "hidden"
       }
-    }, .05, '-=1').from('.breadcrumbs.abt', 0.7, {
+    }, .05, '-=2').from('.breadcrumbs.abt', 0.7, {
       x: -400
     }, '-=2.5');
     var scene = new ScrollMagic.Scene({
       triggerElement: ".about"
     }).setTween(tl2).addTo(controller);
+    scene.triggerHook(0.9);
   };
 
   var characterScrollFX = function characterScrollFX() {
@@ -89,7 +90,7 @@ if (window.innerWidth >= 1024) {
     }, {
       left: 0,
       scale: 1
-    }, .5).staggerFrom(vDescription.lines, t - .3, {
+    }, '-=.5').staggerFrom(vDescription.lines, t - .3, {
       css: {
         left: -200,
         opacity: 0
@@ -101,8 +102,9 @@ if (window.innerWidth >= 1024) {
       x: -400
     }, '-=2.5');
     var scene = new ScrollMagic.Scene({
-      triggerElement: ".characters"
-    }).setTween(tl2).addTo(controller);
+      triggerElement: ".trailer"
+    });
+    scene.triggerHook(0.1).setTween(tl2).addTo(controller);
   };
 
   var wallpaperScrollFX = function wallpaperScrollFX() {
@@ -121,82 +123,112 @@ if (window.innerWidth >= 1024) {
     }, '-=2.5');
     var scene = new ScrollMagic.Scene({
       triggerElement: ".wallpaper"
-    }).setTween(tl3).addTo(controller);
+    });
+    scene.triggerHook(0.9).setTween(tl3).addTo(controller);
   };
 
-  tl.from('header', 10, {
-    ease: Power4.easeOut,
-    css: {
-      opacity: 0
-    }
-  }, 1).fromTo('.trailer-container', .75, {
-    css: {
-      top: "-16rem",
-      height: "61rem",
-      width: "100vw",
-      left: "-33.8rem",
-      filter: "hue-rotate(180deg)"
-    }
-  }, {
-    css: {
-      height: "34rem",
-      top: "-1rem",
-      left: "-33.8rem",
-      filter: "hue-rotate(0deg)"
-    }
-  }, 2.5).from('.hero-container i', .5, {
-    ease: Power2.easeInOut,
-    css: {
-      left: "-33.8rem"
-    }
-  }, "-=7.25").to('.trailer-container', 0.5, {
-    ease: Power2.easeInOut,
-    css: {
-      left: '-4.8rem'
-    }
-  }, "-=7.25").fromTo('.hero', 1, {
+  if (window.innerWidth > 1550) {
+    tl.from('header', 10, {
+      ease: Power4.easeOut,
+      css: {
+        opacity: 0
+      }
+    }, 1).fromTo('.trailer-container', .75, {
+      css: {
+        top: "-16rem",
+        height: "61rem",
+        width: "100vw",
+        left: "-42.8rem",
+        filter: "hue-rotate(180deg)"
+      }
+    }, {
+      css: {
+        height: "42rem",
+        top: "-7rem",
+        left: "-42.8rem",
+        filter: "hue-rotate(0deg)"
+      }
+    }, 2.5).from('.hero-container i', .5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: "-600px"
+      }
+    }, "-=7.25").to('.trailer-container', 0.5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: '-4.8rem'
+      }
+    }, "-=7.25").to('.hero', 0.5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: '-148px'
+      }
+    }, "-=7.25");
+  } else if (window.innerWidth < 1550) {
+    tl.from('header', 10, {
+      ease: Power4.easeOut,
+      css: {
+        opacity: 0
+      }
+    }, 1).fromTo('.trailer-container', .75, {
+      css: {
+        top: "-16rem",
+        height: "61rem",
+        width: "110vw",
+        left: "-38rem",
+        filter: "hue-rotate(180deg)"
+      }
+    }, {
+      css: {
+        height: "35rem",
+        top: "0rem",
+        left: "-33.8rem",
+        filter: "hue-rotate(0deg)"
+      }
+    }, 2.5).from('.hero-container i', .5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: "-600px"
+      }
+    }, "-=7.25").to('.trailer-container', 0.5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: '-8.8rem'
+      }
+    }, "-=7.25").to('.hero', 0.5, {
+      ease: Power2.easeInOut,
+      css: {
+        left: '-248px'
+      }
+    }, "-=7.25");
+  }
+
+  tl.from('.hero', 1, {
     ease: Power2.easeInOut,
     css: {
       left: "300rem",
       opacity: 0
     }
-  }, {
-    css: {
-      left: "-17rem",
-      opacity: 1
-    }
-  }, 3.45).from('.hero-numbers', .9, {
+  }, 3.45).from('.hero-numbers', .7, {
     ease: Power2.easeInOut,
     css: {
-      left: "-300rem",
+      left: "300rem",
       opacity: 0.5
     }
-  }, 3.65).fromTo('.button-container-home', .8, {
+  }, 3.65).from('.button-container-home', .85, {
     ease: Power3.easeInOut,
     css: {
       left: "300rem",
       opacity: 0
     }
-  }, {
-    css: {
-      left: "-17rem",
-      opacity: 1
-    }
-  }, 3.75).fromTo('nav', .8, {
+  }, 3.5).from('nav', .8, {
     ease: Power3.easeInOut,
     css: {
       opacity: 0,
       top: "-100rem"
     }
-  }, {
-    css: {
-      opacity: 1,
-      top: "1rem"
-    }
-  }, 2).fromTo('.trailer-border', 1, {
+  }, 2).from('.trailer-border', .8, {
     left: -5000
-  }, {
-    left: 0
   }, 3.50).staggerFrom(navigation, .75, {
     opacity: 0,
     left: 200
@@ -208,7 +240,13 @@ if (window.innerWidth >= 1024) {
   }, 3.75).from('.next-hero-preview', 1, {
     ease: Power3.easeInOut,
     x: 300
-  }, 3.75);
+  }, 3.75).from('.platform-select', 0.8, {
+    left: 3000
+  }, 4).from('.sm-links', 0.7, {
+    left: -100
+  }, 2.5).from('.platform-spacer', 0.7, {
+    width: 30
+  }, 4);
   var controller = new ScrollMagic.Controller();
   var tl2 = new TimelineMax();
   var scene = new ScrollMagic.Scene({
@@ -227,57 +265,124 @@ var controller = new ScrollMagic.Controller();
 var homePage = new ScrollMagic.Scene({
   triggerElement: 'header',
   duration: document.querySelector('header').offsetHeight
-}).setClassToggle("#home", "active").addTo(controller);
+});
+homePage.triggerHook(0.5).setClassToggle("#home", "active").addTo(controller);
 var aboutPage = new ScrollMagic.Scene({
   triggerElement: '.about',
   duration: document.querySelector('.about').offsetHeight
-}).setClassToggle("#about", "active").addTo(controller);
+});
+aboutPage.triggerHook(0.5).setClassToggle("#about", "active").addTo(controller);
 var trailerPage = new ScrollMagic.Scene({
   triggerElement: '.trailer',
   duration: document.querySelector('.trailer').offsetHeight
-}).setClassToggle("#trailer", "active").addTo(controller);
+});
+trailerPage.triggerHook(0.5).setClassToggle("#trailer", "active").addTo(controller);
 var characterPage = new ScrollMagic.Scene({
   triggerElement: '.characters',
   duration: document.querySelector('.characters').offsetHeight
-}).setClassToggle("#characters", "active").addTo(controller);
+});
+characterPage.triggerHook(0.5).setClassToggle("#characters", "active").addTo(controller);
 var wallpaperPage = new ScrollMagic.Scene({
   triggerElement: '.wallpaper',
   duration: document.querySelector('.wallpaper').offsetHeight
-}).setClassToggle("#wallpaper", "active").addTo(controller);
-new ScrollMagic.Scene({
+});
+wallpaperPage.triggerHook(0.5).setClassToggle("#wallpaper", "active").addTo(controller);
+var burgerChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.1).setClassToggle(".hamburger-menu", "active").addTo(controller);
-new ScrollMagic.Scene({
+burgerChange.triggerHook(0.1).setClassToggle(".hamburger-menu", "active").addTo(controller);
+var logoChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.1).setClassToggle("nav img", "active").addTo(controller);
-new ScrollMagic.Scene({
+logoChange.triggerHook(0.1).setClassToggle("nav img", "active").addTo(controller);
+var twitterChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".twitter-link", "active").addTo(controller);
-new ScrollMagic.Scene({
+twitterChange.triggerHook(0.9).setClassToggle(".twitter-link", "active").addTo(controller);
+var instagranChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".insta-link", "active").addTo(controller);
-new ScrollMagic.Scene({
+instagranChange.triggerHook(0.9).setClassToggle(".insta-link", "active").addTo(controller);
+var facebookChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".facebook-link", "active").addTo(controller);
-new ScrollMagic.Scene({
+facebookChange.triggerHook(0.9).setClassToggle(".facebook-link", "active").addTo(controller);
+var youtubeChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".youtube-link", "active").addTo(controller);
-new ScrollMagic.Scene({
+youtubeChange.triggerHook(0.9).setClassToggle(".youtube-link", "active").addTo(controller);
+var socialChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".sm-links", "active").addTo(controller);
-new ScrollMagic.Scene({
+socialChange.triggerHook(0.9).setClassToggle(".sm-links", "active").addTo(controller);
+var shareBtnChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".share-btn-mobile", "active").addTo(controller);
-new ScrollMagic.Scene({
+shareBtnChange.triggerHook(0.9).setClassToggle(".share-btn-mobile", "active").addTo(controller);
+var scrollChange = new ScrollMagic.Scene({
   triggerElement: '.about'
 });
-changeColor.triggerHook(0.9).setClassToggle(".scroll-indicator", "active").addTo(controller);
+scrollChange.triggerHook(0.9).setClassToggle(".scroll-indicator", "active").addTo(controller); ///        ///////////////////////////////////
+//////////////////////////////////////////// Navigation ///////////////////////////////////
+////////////////////////////////////////////        ///
+//Links
+
+var home = document.querySelector('#home');
+var about = document.querySelector('#about');
+var trailer = document.querySelector('#trailer');
+var characters = document.querySelector('#characters');
+var wallpaper = document.querySelector('#wallpaper'); //Anchors
+
+var homeLink = document.querySelector('#homeLink');
+var aboutLink = document.querySelector('#aboutLink');
+var trailerLink = document.querySelector('#trailerLink');
+var charactersLink = document.querySelector('#charactersLink');
+var wallpaperLink = document.querySelector('#wallpaperLink');
+var links = [home, about, trailer, characters, wallpaper];
+var anchors = [homeLink, aboutLink, trailerLink, charactersLink, wallpaperLink];
+var scrollIndicator = document.querySelector('.scroll-indicator');
+var logo = document.querySelector('nav img');
+logo.addEventListener('click', function () {
+  document.documentElement.scrollTop = 0;
+});
+
+var _loop = function _loop(i) {
+  links[i].addEventListener('click', function () {
+    document.documentElement.scrollTop = getPosition(anchors[i]);
+  });
+};
+
+for (var i = 0; i < links.length; i++) {
+  _loop(i);
+}
+
+function getPosition(el) {
+  var position = 0;
+
+  while (el) {
+    position += el.offsetTop - el.scrollTop + el.clientTop;
+    el = el.offsetParent;
+  }
+
+  return position;
+}
+
+document.addEventListener('scroll', function () {
+  if (wallpaper.classList.contains('active')) {
+    scrollIndicator.style.display = 'none';
+  } else {
+    scrollIndicator.style.display = 'block';
+  }
+});
+scrollIndicator.addEventListener('click', function () {
+  var index;
+
+  for (var i = 0; i < links.length; i++) {
+    if (links[i].classList.contains('active')) {
+      index = links.indexOf(links[i]);
+    }
+  }
+
+  document.documentElement.scrollTop = getPosition(anchors[index + 1]);
+});
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -19,46 +19,97 @@ var tl = new TimelineMax()
 
 if ( window.innerWidth >= 1024 ) {
 
-    tl.from('header', 10, {
-        ease: Power4.easeOut,
-        css:{
-            opacity: 0,
+    if (window.innerWidth > 1550) {
+        tl.from('header', 10, {
+            ease: Power4.easeOut,
+            css:{
+                opacity: 0,
+            }
+        },1)
+        .fromTo('.trailer-container', .75,{
+            css:{
+                top: "-16rem",
+                height: "61rem",
+                width: "100vw",
+                left: "-42.8rem",
+                filter: "hue-rotate(180deg)"
+            }
+        },{css:{
+            height: "42rem",
+            top: "-7rem",
+            left: "-42.8rem",
+            filter: "hue-rotate(0deg)"
         }
-    },1)
-    .fromTo('.trailer-container', .75,{
-        css:{
-            top: "-16rem",
-            height: "61rem",
-            width: "100vw",
-            left: "-33.8rem",
-            filter: "hue-rotate(180deg)"
-        }
-    },{css:{
-        height: "34rem",
-        top: "-1rem",
-        left: "-33.8rem",
-        filter: "hue-rotate(0deg)"
-    }
-    },2.5)
-    .from('.hero-container i', .5, {
-    ease: Power2.easeInOut,
-    css:{
-        left: "-33.8rem"
-    }},"-=7.25")
-    .to('.trailer-container', 0.5, {
+        },2.5)
+        .from('.hero-container i', .5, {
         ease: Power2.easeInOut,
         css:{
-            left: '-4.8rem'
+            left: "-600px"
+        }},"-=7.25")
+        .to('.trailer-container', 0.5, {
+            ease: Power2.easeInOut,
+            css:{
+                left: '-4.8rem'
+            }
+        },"-=7.25")
+        .to('.hero', 0.5, {
+            ease: Power2.easeInOut,
+            css:{
+                left: '-148px'
+            }
+        },"-=7.25")
+    } else if ( window.innerWidth < 1550 ) {
+        tl.from('header', 10, {
+            ease: Power4.easeOut,
+            css:{
+                opacity: 0,
+            }
+        },1)
+        .fromTo('.trailer-container', .75,{
+            css:{
+                top: "-16rem",
+                height: "61rem",
+                width: "110vw",
+                left: "-38rem",
+                filter: "hue-rotate(180deg)"
+            }
+        },{css:{
+            height: "35rem",
+            top: "0rem",
+            left: "-33.8rem",
+            filter: "hue-rotate(0deg)"
         }
-    },"-=7.25")
-    .fromTo('.hero', 1, {ease: Power2.easeInOut,css:{left:"300rem",opacity:0}},{css:{left:"-17rem",opacity:1}},3.45)
-    .from('.hero-numbers', .9, {ease: Power2.easeInOut,css:{left:"-300rem", opacity: 0.5}},3.65)
-    .fromTo('.button-container-home', .8, {ease: Power3.easeInOut, css:{left:"300rem",opacity:0}},{css:{left:"-17rem",opacity:1}},3.75)
-    .fromTo('nav', .8, {ease: Power3.easeInOut, css:{opacity: 0, top:"-100rem"}},{css:{opacity: 1, top:"1rem"}},2)
-    .fromTo('.trailer-border', 1, {left:-5000}, {left:0}, 3.50)
+        },2.5)
+        .from('.hero-container i', .5, {
+        ease: Power2.easeInOut,
+        css:{
+            left: "-600px"
+        }},"-=7.25")
+        .to('.trailer-container', 0.5, {
+            ease: Power2.easeInOut,
+            css:{
+                left: '-8.8rem'
+            }
+        },"-=7.25")
+        .to('.hero', 0.5, {
+            ease: Power2.easeInOut,
+            css:{
+                left: '-248px'
+            }
+        },"-=7.25")        
+    }
+
+    tl.from('.hero', 1, {ease: Power2.easeInOut,css:{left:"300rem",opacity:0}},3.45)
+    .from('.hero-numbers', .7, {ease: Power2.easeInOut,css:{left:"300rem", opacity: 0.5}},3.65)
+    .from('.button-container-home', .85, {ease: Power3.easeInOut, css:{left:"300rem",opacity:0}},3.5)
+    .from('nav', .8, {ease: Power3.easeInOut, css:{opacity: 0, top:"-100rem"}},2)
+    .from('.trailer-border', .8, {left:-5000}, 3.50)
     .staggerFrom(navigation, .75, {opacity: 0, left: 200}, 0.2, 1.5)
     .from('.hero-divider', .8, {ease: Power3.easeInOut,css:{right:"-111%"}}, 3.75)
     .from('.next-hero-preview', 1, {ease: Power3.easeInOut,x:300}, 3.75)
+    .from('.platform-select', 0.8, {left:3000}, 4)
+    .from('.sm-links', 0.7, {left: -100}, 2.5)
+    .from('.platform-spacer', 0.7, {width: 30}, 4)
     
     var controller = new ScrollMagic.Controller();
     var tl2 = new TimelineMax();
@@ -83,19 +134,20 @@ if ( window.innerWidth >= 1024 ) {
                 left: "-70px",
                 color: 'black',
                 opacity: 0,
-            }}, 0.02, "-=1")
+            }}, 0.02, "-=2")
             .staggerFrom(splitParagraph.lines, 1, {
                 ease: Power2.easeInOut,
                 css:{
                     opacity: 0,
                     height: 0,
-                    overflow: "hidden"}}, .05, '-=1')
+                    overflow: "hidden"}}, .05, '-=2')
             .from('.breadcrumbs.abt', 0.7, {x:-400},'-=2.5')
         var scene = new ScrollMagic.Scene({
             triggerElement: ".about"
         })
     .setTween(tl2)
     .addTo(controller)
+    scene.triggerHook(0.9)
     }
     aboutScrollFX()
     function characterScrollFX() {
@@ -135,7 +187,7 @@ if ( window.innerWidth >= 1024 ) {
             }},{
                 left: 0,
                 scale: 1,
-            },.5)
+            },'-=.5')
         .staggerFrom(vDescription.lines, t-.3, {      
             css:{
                 left: -200,
@@ -146,8 +198,9 @@ if ( window.innerWidth >= 1024 ) {
         .from('.breadcrumbs.char', 0.7, {x:-400},'-=2.5')
         
         var scene = new ScrollMagic.Scene({
-            triggerElement: ".characters"
+            triggerElement: ".trailer"
         })
+        scene.triggerHook(0.1)
         .setTween(tl2)
         .addTo(controller)
     }
@@ -166,6 +219,7 @@ if ( window.innerWidth >= 1024 ) {
         var scene = new ScrollMagic.Scene({
             triggerElement: ".wallpaper"
         })
+        scene.triggerHook(0.9)
         .setTween(tl3)
         .addTo(controller)
     }
@@ -175,69 +229,134 @@ if ( window.innerWidth >= 1024 ) {
 var controller = new ScrollMagic.Controller();
 
 var homePage = new ScrollMagic.Scene({triggerElement: 'header', duration: document.querySelector('header').offsetHeight })
+    homePage.triggerHook(0.5)
     .setClassToggle("#home", "active") 
     .addTo(controller);
 
 var aboutPage = new ScrollMagic.Scene({triggerElement: '.about',duration: document.querySelector('.about').offsetHeight})
+    aboutPage.triggerHook(0.5)
     .setClassToggle("#about", "active") 
     .addTo(controller);
 
 var trailerPage = new ScrollMagic.Scene({triggerElement: '.trailer',duration: document.querySelector('.trailer').offsetHeight})
+    trailerPage.triggerHook(0.5)
     .setClassToggle("#trailer", "active") 
     .addTo(controller);
 
 var characterPage = new ScrollMagic.Scene({triggerElement: '.characters',duration: document.querySelector('.characters').offsetHeight})
+    characterPage.triggerHook(0.5)
     .setClassToggle("#characters", "active") 
     .addTo(controller);
 
 var wallpaperPage = new ScrollMagic.Scene({triggerElement: '.wallpaper',duration: document.querySelector('.wallpaper').offsetHeight})
+    wallpaperPage.triggerHook(0.5)
     .setClassToggle("#wallpaper", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.1)
+let burgerChange = new ScrollMagic.Scene({triggerElement:'.about'})
+burgerChange.triggerHook(0.1)
     .setClassToggle(".hamburger-menu", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.1)
+let logoChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    logoChange.triggerHook(0.1)
     .setClassToggle("nav img", "active") 
     .addTo(controller);
     
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let twitterChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    twitterChange.triggerHook(0.9)
     .setClassToggle(".twitter-link", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let instagranChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    instagranChange.triggerHook(0.9)
     .setClassToggle(".insta-link", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let facebookChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    facebookChange.triggerHook(0.9)
     .setClassToggle(".facebook-link", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let youtubeChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    youtubeChange.triggerHook(0.9)
     .setClassToggle(".youtube-link", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let socialChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    socialChange.triggerHook(0.9)
     .setClassToggle(".sm-links", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let shareBtnChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    shareBtnChange.triggerHook(0.9)
     .setClassToggle(".share-btn-mobile", "active") 
     .addTo(controller);
 
-new ScrollMagic.Scene({triggerElement:'.about'})
-    changeColor.triggerHook(0.9)
+let scrollChange = new ScrollMagic.Scene({triggerElement:'.about'})
+    scrollChange.triggerHook(0.9)
     .setClassToggle(".scroll-indicator", "active") 
     .addTo(controller);
 
+                                              ///        ///////////////////////////////////
+//////////////////////////////////////////// Navigation ///////////////////////////////////
+////////////////////////////////////////////        ///
 
+//Links
+const home = document.querySelector('#home')
+const about = document.querySelector('#about')
+const trailer = document.querySelector('#trailer')
+const characters = document.querySelector('#characters')
+const wallpaper = document.querySelector('#wallpaper')
 
+//Anchors
+const homeLink = document.querySelector('#homeLink')
+const aboutLink = document.querySelector('#aboutLink')
+const trailerLink = document.querySelector('#trailerLink')
+const charactersLink = document.querySelector('#charactersLink')
+const wallpaperLink = document.querySelector('#wallpaperLink')
+
+const links = [home, about, trailer, characters, wallpaper]
+const anchors = [homeLink, aboutLink, trailerLink, charactersLink, wallpaperLink]
+
+let scrollIndicator = document.querySelector('.scroll-indicator')
+let logo = document.querySelector('nav img')
+
+logo.addEventListener('click', function(){
+    document.documentElement.scrollTop = 0;
+})
+
+for (let i=0; i<links.length; i++) {
+
+    links[i].addEventListener('click', function(){
+
+        document.documentElement.scrollTop = getPosition(anchors[i])
+    })
+}
+
+function getPosition(el){
+    let position = 0
+    while(el){
+        position += (el.offsetTop - el.scrollTop + el.clientTop);
+        el = el.offsetParent
+    }
+    return position
+}
+
+document.addEventListener('scroll', function(){
+    if ( wallpaper.classList.contains('active') ) {
+        scrollIndicator.style.display = 'none'
+    } else {
+        scrollIndicator.style.display = 'block'
+    }
+})
+
+scrollIndicator.addEventListener('click', function(){
+    let index
+    for (let i=0; i<links.length; i++){
+        if (links[i].classList.contains('active')) {
+            index = links.indexOf(links[i])
+        }
+    }
+    document.documentElement.scrollTop = getPosition(anchors[index+1])
+})
